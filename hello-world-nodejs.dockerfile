5611e65d3ea2ffa2de3d32ef3d1236b4526c47d2
sebastian_andreoletti <andreole@ar.ibm.com>
2023-01-18 11:57:34 -0300

    pendientes

--- hello-world-nodejs.dockerfile
+++ hello-world-nodejs.dockerfile
@@ -2,8 +2,8 @@
 FROM  registry.redhat.io/ubi9/ubi:latest
 #
 ENV   NODE_VERSION="18/common"
-ENV   MSG_DEMO="**** Hola ****"
-ENV   PORT="3000"
+ENV   MSG_DEMO="**** NO HAY MENSAJE ****"
+ENV   PORT="3002"
 #
 RUN    whoami && dnf upgrade -y \
     && dnf module list nodejs \
@@ -12,6 +12,7 @@ RUN    whoami && dnf upgrade -y \
     && mkdir -p /app/src && chown -R 1001:0 /app/src
 #
 WORKDIR  /app/src
+#
 ADD  package.json .
 ADD  index.js     .
 ##
--- index.js
+++ index.js
@@ -7,8 +7,10 @@ const MongoClient = require('mongodb').MongoClient;
 let databaseMongo = false ;
 try {
   MongoClient.connect("mongodb://demo:123456@localhost:27017/?authMechanism=DEFAULT&authSource=demo", function(err, db) {
-    if(!err) {
-      console.log("Conectado....");
+    if(err) {
+      console.log("...ERROR: MongoDB: ",err,";") ;
+    } else {
+      console.log("MongoDB::Conectado!");
       databaseMongo = db ;
     }
   });
@@ -33,20 +35,30 @@ const server = http.createServer((req, res) => {
   if ( databaseMongo!==false ){
     try {
       let demoDB      = databaseMongo.db("demo") ;
-      let usuarioColl = demoDB.collection("usuarios") ;
-      usuarioColl.find().toArray(function(err, items) {
-         console.log("items: ",items,";") ;
-      });
+      let usuarioColl = demoDB.collection("mensajes") ;
+      //
+      usuarioColl.find({_id: "HOLA_MUNDO" }).toArray(function(err, arrayMensajes) {
+        //
+        if ( err==undefined ){
+        if ( arrayMensajes.length>0 ){
+          msg = arrayMensajes[0].texto ;
+          } ;
+          //
+          console.log("...msg: ",msg) ;
+          //
+        } ;
+        //
+        res.end(msg); 
+        //
+      }) ;
+      //
     } catch(errDB){
       console.log("***ERROR: ",errDB) ;
-      console.log("....demoDB: ",demoDB) ;
     } ;
   } ;
   //
-  res.end(msg);
-  //
 }) ;
-
+//
 server.listen(port, () => {
   console.log(`Ejecutandose en http://localhost:${port}/`);
 });
